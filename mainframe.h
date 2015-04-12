a970c8268809fea6234029c18089738bad70717f
marinadaschenko <marinadaschenko@users.noreply.github.com>
2015-04-12 18:21:46 +0200

    Update mainframe.h

--- mainframe.h
+++ mainframe.h
@@ -1,34 +1,37 @@
- 
-
 #ifndef MAINFRAME_H
 #define MAINFRAME_H
 
 #include <wx/wx.h>
 #include <wx/image.h>
-
  
 
-class MainFrame: public wxFrame {
+class MainFrame : public wxFrame {
 public:
-     
+ 
 
-    MainFrame(wxWindow* parent, int id, const wxString& title, const wxPoint& pos=wxDefaultPosition, const wxSize& size=wxDefaultSize, long style=wxDEFAULT_FRAME_STYLE);
+	MainFrame(wxWindow* parent, int id, const wxString& title, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_FRAME_STYLE);
 
 private:
-    
-    void set_properties();
-    void do_layout();
-     
+	 
+	void set_properties();
+	void do_layout();
+	 
+	 
+protected:
+ 
+	wxStaticText* label_1;
+	wxTextCtrl* tPath;
+	wxButton* btnPath;
+	wxButton* btnClean;
+	 
+
+	DECLARE_EVENT_TABLE();
 
 public:
-    
-    wxStaticText* label_1;
-    wxTextCtrl* tPath;
-    wxButton* btnPath;
-    wxButton* btnClean;
-
- 
-}; 
+	void btnPathClick(wxCommandEvent &event); // wxGlade: <event_handler>
+	void btnCleanClick(wxCommandEvent &event); // wxGlade: <event_handler>
+};  
 
 
+#endif  
  

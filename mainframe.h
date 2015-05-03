7b4295c7e655e71f22074f326b3eb10e0086a06a
marinadaschenko <marinadaschenko@users.noreply.github.com>
2015-05-03 22:37:51 +0300

    Update mainframe.h

--- mainframe.h
+++ mainframe.h
@@ -1,38 +1,39 @@
+ 
+
 #ifndef MAINFRAME_H
 #define MAINFRAME_H
-
 
 #include <wx/wx.h>
 #include <wx/image.h>
  
 
-class MainFrame : public wxFrame {
-public:
- 
 
-	MainFrame(wxWindow* parent, int id, const wxString& title, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxDEFAULT_FRAME_STYLE);
+class MainFrame: public wxFrame {
+public:
+    
+
+    MainFrame(wxWindow* parent, int id, const wxString& title, const wxPoint& pos=wxDefaultPosition, const wxSize& size=wxDefaultSize, long style=wxDEFAULT_FRAME_STYLE);
 
 private:
-	 
-	void set_properties();
-	void do_layout();
-	 
-	 
-protected:
  
-	wxStaticText* label_1;
-	wxTextCtrl* tPath;
-	wxButton* btnPath;
-	wxButton* btnClean;
-	 
+    void set_properties();
+    void do_layout();
+     
 
-	DECLARE_EVENT_TABLE();
+protected:
+    
+    wxStaticText* label_1;
+    wxTextCtrl* tPath;
+    wxButton* btnPath;
+    wxButton* btnClean;
+  
+
+    DECLARE_EVENT_TABLE();
 
 public:
-	void btnPathClick(wxCommandEvent &event); // wxGlade: <event_handler>
-	void btnCleanClick(wxCommandEvent &event); // wxGlade: <event_handler>
-};  
+    void btnPathClick(wxCommandEvent &event); // wxGlade: <event_handler>
+    void btnCleanClick(wxCommandEvent &event); // wxGlade: <event_handler>
+}; // wxGlade: end class
 
 
-#endif  
- 
+#endif // MAINFRAME_H
